517d4d721b4c303a9e19233b5f9c1585a5ae406c
khalilwallakhilo <khalil.salaheddine@outlook.com>
2024-07-01 13:25:27 +0300

    update

--- todolistwithdatabase/Middleware/GlobalExceptionHandlingMiddleware.cs
+++ todolistwithdatabase/Middleware/GlobalExceptionHandlingMiddleware.cs
@@ -2,43 +2,39 @@
 using System.Net;
 using System.Text.Json;
 
-namespace todolistwithdatabase.Middleware  {
-    public class GlobalExceptionHandlingMiddleware : IMiddleware {
-        
+namespace todolistwithdatabase.Middleware
+{
+    public class GlobalExceptionHandlingMiddleware : IMiddleware
+    {
         private readonly ILogger<GlobalExceptionHandlingMiddleware> _logger;
 
-
         public GlobalExceptionHandlingMiddleware(
-            ILogger<GlobalExceptionHandlingMiddleware> logger)=>
-            _logger = logger;
-        
-
-
+            ILogger<GlobalExceptionHandlingMiddleware> logger) => _logger = logger;
         public async Task InvokeAsync(HttpContext context, RequestDelegate next)
         {
-
             try
             {
                 await next(context);
             }
-            catch (Exception ex)
+            catch (Exception e)
             {
-                _logger.LogError(ex, ex.Message);
+                _logger.LogError(e, e.Message);
                 context.Response.StatusCode = (int)HttpStatusCode.InternalServerError;
-            }
+
             ProblemDetails problem = new()
             {
                 Status = (int)HttpStatusCode.InternalServerError,
                 Type = "Server Error",
                 Title = "Server Error",
-                Detail = "An internal server error has occured"
+                    Detail = "An internal server has occured"
+                };
 
-            };
-            var json = JsonSerializer.Serialize(problem);
+                string json = JsonSerializer.Serialize(problem);
 
             await context.Response.WriteAsync(json);
 
             context.Response.ContentType = "application/json";
         }
     }
+}
 }
